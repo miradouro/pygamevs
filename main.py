9dedc57e33537df6ac3faee720aaca1389be02cc
Miradouro <rafaelmiradouro@gmail.com>
2021-03-24 09:00:28 -0300

    segundo commit

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
 import pygame
 import os
 
-WIDTH, HEIGHT = 1280, 720
-WIN = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Tim Game !!!")
+# Padrao de cores
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+YELLOW = (255, 255, 0)
+# Condigurações da Tela
+WIDTH, HEIGHT = 1280, 720
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+SPACE = pygame.transform.scale(pygame.image.load(os.path.join('Assets', 'space.png')), (WIDTH, HEIGHT))
+pygame.display.set_caption("Tim Game !!!")
 BORDER = pygame.Rect(WIDTH//2-5, 0, 10, HEIGHT)
+# FPS e Velocidades do Jogo
 FPS = 60
 VEL = 5
-BULLET_VEL = 7
+BULLET_VEL = 10
+MAX_BULLETS = 5
 SPACESHIP_WIDTH, SPACESHIP_HEIGHT = 55, 40
+YELLOW_HIT = pygame.USEREVENT + 1
+RED_HIT = pygame.USEREVENT + 2
 
 YELLOW_SPACESHIP_IMAGE = pygame.image.load(
     os.path.join('Assets', 'spaceship_yellow.png'))
@@ -22,11 +31,19 @@ RED_SPACESHIP = pygame.transform.rotate(pygame.transform.scale(
     RED_SPACESHIP_IMAGE, (SPACESHIP_WIDTH, SPACESHIP_HEIGHT)), 270)
 
 
-def draw_window(red, yellow):
-    WIN.fill(WHITE)
+def draw_window(red, yellow, yellow_bullets, red_bullets):
+    #WIN.fill(WHITE)
+    WIN.blit(SPACE, (0, 0))
     pygame.draw.rect(WIN, BLACK, BORDER)
     WIN.blit(YELLOW_SPACESHIP, (yellow.x, yellow.y))
     WIN.blit(RED_SPACESHIP, (red.x, red.y))
+
+    for bullet in red_bullets:
+        pygame.draw.rect(WIN, RED, bullet)
+
+    for bullet in yellow_bullets:
+        pygame.draw.rect(WIN, YELLOW, bullet)
+
     pygame.display.update()
 
 
@@ -52,9 +69,31 @@ def red_handle_movement(keys_pressed, red):
         red.y += VEL
 
 
+def handle_bullets(yellow_bullets, red_bullets, yellow, red):
+    for bullet in yellow_bullets:
+        bullet.x += BULLET_VEL
+        if red.colliderect(bullet):
+            pygame.event.post(pygame.event.Event(RED_HIT))
+            yellow_bullets.remove(bullet)
+        elif bullet.x > WIDTH:
+            yellow_bullets.remove(bullet)
+
+    for bullet in red_bullets:
+        bullet.x -= BULLET_VEL
+        if yellow.colliderect(bullet):
+            pygame.event.post(pygame.event.Event(YELLOW_HIT))
+            red_bullets.remove(bullet)
+        elif bullet.x < 0:
+            red_bullets.remove(bullet)
+
+
+
 def main():
     yellow = pygame.Rect(100, 300, SPACESHIP_WIDTH, SPACESHIP_HEIGHT)
     red = pygame.Rect(1135, 300, SPACESHIP_WIDTH, SPACESHIP_HEIGHT)
+
+    red_bullets = []
+    yellow_bullets = []
 
     clock = pygame.time.Clock()
     run = True
@@ -63,11 +102,19 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LCTRL and len(yellow_bullets) < MAX_BULLETS:
+                    bullet = pygame.Rect(yellow.x + yellow.width, yellow.y + yellow.height//2 - 2, 10, 4)
+                    yellow_bullets.append(bullet)
+                if event.key == pygame.K_RCTRL and len(red_bullets) < MAX_BULLETS:
+                    bullet = pygame.Rect(red.x, red.y + red.height//2 - 2, 10, 4)
+                    red_bullets.append(bullet)
 
+        handle_bullets(yellow_bullets, red_bullets, yellow, red)
         keys_pressed = pygame.key.get_pressed()
         yellow_handle_movement(keys_pressed, yellow)
         red_handle_movement(keys_pressed, red)
-        draw_window(red, yellow)
+        draw_window(red, yellow, yellow_bullets, red_bullets)
     pygame.quit()
 
 
